2afc70a4757fce5679bb07571b1b942d7cd23b50
Rene Olivo <rene.olivo.do@gmail.com>
2020-03-24 01:25:48 +0000

    RSE-1033: Add option to get active and inactive case types

--- ang/test/civicase-base/services/case-type.service.spec.js
+++ ang/test/civicase-base/services/case-type.service.spec.js
@@ -1,24 +1,48 @@
 /* eslint-env jasmine */
 
-(() => {
+((_) => {
   describe('Case Type', () => {
-    let CaseType, CaseTypesData;
+    let CaseType, CaseTypesData, CaseTypesMockDataProvider;
 
-    beforeEach(module('civicase', 'civicase.data'));
+    beforeEach(module('civicase', 'civicase.data', (_CaseTypesMockDataProvider_) => {
+      CaseTypesMockDataProvider = _CaseTypesMockDataProvider_;
+
+      CaseTypesMockDataProvider.add({
+        title: 'inactive case type',
+        is_active: '0'
+      });
+    }));
+
+    afterEach(() => {
+      CaseTypesMockDataProvider.restore();
+    });
 
     beforeEach(inject((_CaseType_, _CaseTypesMockData_) => {
       CaseType = _CaseType_;
       CaseTypesData = _CaseTypesMockData_.get();
     }));
 
-    describe('when getting all case types', () => {
-      let returnedCaseTypes;
+    describe('when getting all active case types', () => {
+      let activeCaseTypes, returnedCaseTypes;
 
       beforeEach(() => {
+        activeCaseTypes = _.pick(CaseTypesData, (caseType) => caseType.is_active === '1');
         returnedCaseTypes = CaseType.getAll();
       });
 
-      it('returns all the case types', () => {
+      it('returns all the active case types', () => {
+        expect(returnedCaseTypes).toEqual(activeCaseTypes);
+      });
+    });
+
+    describe('when getting all case including inactive ones', () => {
+      let returnedCaseTypes;
+
+      beforeEach(() => {
+        returnedCaseTypes = CaseType.getAll({ includeInactive: true });
+      });
+
+      it('returns all the active case types', () => {
         expect(returnedCaseTypes).toEqual(CaseTypesData);
       });
     });
@@ -41,4 +65,4 @@
       });
     });
   });
-})();
+})(CRM._);
